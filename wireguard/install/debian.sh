710d475d7ddebf4b2226f3f6f8c33d321733e98a
Frédérick SIMON <frederick.simon@outlook.com>
2022-08-16 23:20:24 +0200

    update debian

--- wireguard/install/debian.sh
+++ wireguard/install/debian.sh
@@ -75,7 +75,6 @@ log "Setting up wiregard enviroment"
 _wg_server_private=`wg genkey`
 log "Clé privée : $_wg_server_private"
 _wg_server_public=`echo "$_wg_server_private" | wg pubkey`
-eval $_wg_server_public
 log "Clé public : $_wg_server_public"
 
 echo "[Interface]\nPrivateKey = $_wg_server_private # la clé privée du serveur\nAddress = 10.206.0.1 # l'adresse du sous réseau\nPostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE\nPostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE\nListenPort = 51820 # le port d'écoute par défaut" > /etc/wireguard/wg0.conf
