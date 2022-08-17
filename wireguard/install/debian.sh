09d033f2c82559873d46e5e62396e981067e7e54
Frédérick SIMON <frederick.simon@outlook.com>
2022-08-17 11:48:32 +0200

    update wg0.conf

--- wireguard/install/debian.sh
+++ wireguard/install/debian.sh
@@ -85,7 +85,7 @@ sed -i 's,<server-privatekey>,'"$_wg_server_private"',g' /etc/wireguard/wg0.conf
 
 wget --no-cache -P /etc/wireguard $_raw_base/install/server-key
 sed -i 's,<server-privatekey>,'"$_wg_server_private"',g' /etc/wireguard/server-key
-sed -i 's,<server-privatekey>,'"$_wg_server_private"',g' /etc/wireguard/server-key
+sed -i 's,<server-privatekey>,'"$_wg_server_public"',g' /etc/wireguard/server-key
 
 
 chown -R root:root /etc/wireguard
